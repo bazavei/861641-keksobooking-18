8c442dcfe344c32b43aceddfcedfcf357135fc9d
Ekaterina <bazavei94@ gmail.com>
2019-10-10 14:17:58 +0700

    Осуществляет возможность перемещения mainPin

--- js/map.js
+++ js/map.js
@@ -47,6 +47,42 @@
     setPinCoordinate(true);
   };
 
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
   mainPin.addEventListener('mousedown', onMainPinClick);
   mainPin.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, onMainPinClick);
