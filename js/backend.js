0c0817bf95cd12ccf12bc4d5171afda1ba127001
Ekaterina <bazavei94@ gmail.com>
2019-10-16 22:07:51 +0700

    Выполняет задание №15

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,12 @@
 'use strict';
 
 (function () {
-  var load = function (onLoad, onError) {
-    var URL = 'https://js.dump.academy/keksobooking/data';
+  var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
+  var URL_SAVE = 'https://js.dump.academy/keksobooking';
 
+  var createXHR = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onLoad(xhr.response);
@@ -24,38 +24,19 @@
     });
 
     xhr.timeout = 10000;
+    return xhr;
+  };
 
-    xhr.open('GET', URL);
+  var load = function (onLoad, onError) {
+    var xhr = createXHR(onLoad, onError);
+    xhr.open('GET', URL_LOAD);
     xhr.send();
   };
 
   var save = function (data, onLoad, onError) {
-    var URL = 'https://js.dump.academy/keksobooking';
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-
-    xhr.open('POST', URL);
+    var xhr = createXHR(onLoad, onError);
+    xhr.open('POST', URL_SAVE);
     xhr.send(data);
-
   };
 
   window.backend = {
